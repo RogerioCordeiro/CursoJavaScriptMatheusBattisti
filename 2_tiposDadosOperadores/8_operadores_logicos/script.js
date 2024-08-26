3ea57d2c16a29b05947bf3e4b519139d9f3147f7
RogerioCordeiro <rocordeirosilva@gmail.com>
2024-08-26 13:21:22 -0300

    Aula 26 - Operadores de compração E e OU.

---
+++ 2_tiposDadosOperadores/8_operadores_logicos/script.js
@@ -0,0 +1,6 @@
+console.log(5 > 3 && 3 == 2)
+console.log(5 > 3 || 3 == 1)
+
+console.log(3 == 3 && "Rogerio" == "Rogerio")
+console.log("Felipe" == "João" || false)
+console.log(!(!(true && true)))
