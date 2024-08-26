6a3eecd0118aef1022052ebb146008953761bcc9
RogerioCordeiro <rocordeirosilva@gmail.com>
2024-08-26 15:46:57 -0300

    Secao 4 - Uso de variaveis em javascript

---
+++ 4_estruturasDeProgramacao/1_variaveis/script.js
@@ -0,0 +1,19 @@
+let nome = "Rogerio"
+
+console.log(nome)
+console.log(`O meu nome é ${nome}`)
+
+let laranjas = 5;
+console.log(laranjas * laranjas)
+
+nome = "João"
+
+console.log(nome)
+
+laranjas = 82325
+
+console.log(laranjas)
+
+laranjas = "Laranja"
+
+console.log(laranjas)
