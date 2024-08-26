2719f2aa77e0741f66d643f75dab31f67a4566ff
RogerioCordeiro <rocordeirosilva@gmail.com>
2024-08-26 14:30:28 -0300

    Secao3 Exercicios - Commit do primeiro exercicio.

---
+++ 3_Exercicios/ex_1/script.js
@@ -0,0 +1,3 @@
+console.log("Esse texto foi escrito com aspas duplas")
+console.log('Esse texto foi escrito com aspas simples')
+console.log(`Esse texto utilou template string para somar 5 e 3 que é = ${3 + 5}`)
