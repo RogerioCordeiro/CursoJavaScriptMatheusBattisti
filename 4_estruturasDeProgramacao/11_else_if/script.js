398a7d7162fdc75fa07d83dff7f315069a0adba1
RogerioCordeiro <rocordeirosilva@gmail.com>
2024-08-27 10:05:36 -0300

    Aula 59 - uso de if, else if e else

---
+++ 4_estruturasDeProgramacao/11_else_if/script.js
@@ -0,0 +1,11 @@
+let nome = "Rogerio";
+
+if (nome != undefined) {
+    console.log("Nome está definido")
+}
+else if (nome == "Rogerio" && nome.length > 5) {
+    console.log('Nome é Rogerio')
+}
+else {
+    console.log("nome não é Rogerio")
+}
