6d08c58f1ace2a7e3f6a2cc33149e07756fe38f0
RogerioCordeiro <rocordeirosilva@gmail.com>
2024-08-27 10:46:43 -0300

    Aula 67 - formas de incrementar uma variavel de controle.

---
+++ 4_estruturasDeProgramacao/18_resumo_incr/script.js
@@ -0,0 +1,31 @@
+let tituloAula = "Aula 67 - Formas de Incremento"
+let escreverH1 = document.getElementById("escrever")
+escreverH1.textContent = tituloAula
+
+
+let container = document.getElementById('container')
+
+
+// let h2 = document.createElement("h2")
+// h2.innerHTML = `O z é ${z}`
+// container.appendChild(h2)
+// let nome = "João"
+let numero = 0
+while (numero < 50) {
+
+    numero += 10
+    let paragrafo = document.createElement("p")
+
+    paragrafo.textContent = `${numero}`
+    container.appendChild(paragrafo)
+}
+
+for (let i = 0; i < 10; i++) {
+
+    let paragrafo = document.createElement("p")
+
+    paragrafo.textContent = `O i é ${i}`
+    container.appendChild(paragrafo)
+}
+
+
